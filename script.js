0f213d043ffc9b3f41cf68a48bc68d0e92ab39f4
Julian Reimer <j.reimer@showme-stores.com>
2026-08-26 12:52:34 +0200

    Tifteli und Kapitel aktualisiert

--- script.js
+++ script.js
@@ -13,13 +13,20 @@ const sucheErgebnisse = document.getElementById("sucheErgebnisse");
 const rezepte = [
     { name: "Gyros Suppe", url: "gyrossuppe.html", kapitel: "Unsere Klassiker" },
     { name: "Rindergulasch", url: "rindergulasch.html", kapitel: "Unsere Klassiker" },
+    { name: "Italienische Steakpfanne", url: "italienische-steakpfanne.html", kapitel: "Unsere Klassiker" },
+    { name: "Tifteli", url: "tifteli.html", kapitel: "Unsere Klassiker" },
+
     { name: "Überbackene Brezeln", url: "ueberbackene-brezeln.html", kapitel: "Was Kleines dazu" },
     { name: "Dillgurken", url: "dillgurken.html", kapitel: "Was Kleines dazu" },
+
     { name: "Kräuterdressing", url: "kraeuterdressing.html", kapitel: "Das macht den Unterschied" },
     { name: "Rahmsoße", url: "rahmsosse.html", kapitel: "Das macht den Unterschied" },
     { name: "Remoulade", url: "remoulade.html", kapitel: "Das macht den Unterschied" },
     { name: "Big Mac Sauce", url: "big-mac-sauce.html", kapitel: "Das macht den Unterschied" },
+
     { name: "Pluschki", url: "pluschki.html", kapitel: "Was Süßes aus dem Ofen" },
+    { name: "Donauwelle", url: "donauwelle.html", kapitel: "Was Süßes aus dem Ofen" },
+
     { name: "Tiramisu", url: "tiramisu.html", kapitel: "Ein bisschen Platz ist noch" },
     { name: "Lemon Curd Tiramisu", url: "lemon-curd-tiramisu.html", kapitel: "Ein bisschen Platz ist noch" },
     { name: "Pick Up Dessert", url: "pick-up-dessert.html", kapitel: "Ein bisschen Platz ist noch" },
